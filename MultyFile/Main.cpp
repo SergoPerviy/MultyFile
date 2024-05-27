eb19cbd5933551d4a9a5259f135047e3b747fa60
К - 7 < - 7@WIN-US3KR26T6U6>
2024-05-27 20:59:39 +0500

    Добавьте файлы проекта.

---
+++ MultyFile/Main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Functions.h"
+#include "Structers.hpp"
+#include "Data.hpp"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	say_hi();
+
+	std::cout << new_str(4, '!') << std::endl;
+
+	const int size = 4;
+	int arr[size]{ 1, 2, 3, 4 };
+	print_arr(arr, size);
+
+	Person p1{ "Bob", 33, "Gay" };
+	print_person(p1);
+
+	_A = 7;
+	std::cout << "A = " << _A << std::endl;
+	std::cout << "PI = " << _PI << std::endl;
+	print_arr(_ARR, _SIZE);
+
+	return 0;
+}
---
+++ MultyFile/Structers.cpp
@@ -0,0 +1,8 @@
+#include "Structers.hpp"
+#include <iostream>
+
+void print_person(const Person& P){
+	std::cout << "Name: " << P.name << std::endl;
+	std::cout << "Age: " << P.age << std::endl;
+	std::cout << "Job: " << P.job << std::endl;
+ }
